bb28b05f9d36e23875cc1c0440dd0f9860ce77bf
daliciojunior <daliciojunior@gmail.com>
2022-08-30 07:03:37 -0300

    Spring Boot starter files set up

---
+++ src/test/java/com/daliciojunior/javatdd/JavaTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daliciojunior.javatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
